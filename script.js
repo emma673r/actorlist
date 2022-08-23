95973c8aceb5a3d8c11ef335424d89dcad830f38
emma673r <90177139+emma673r@users.noreply.github.com>
2022-08-23 13:57:00 +0200

    first commit actor list

---
+++ script.js
@@ -0,0 +1,21 @@
+const url = src="actors.json";
+const main = document.getElementsByTagName("main");
+
+window.addEventListener("DOMContentLoaded", start);
+ function start() {
+    fetchData();
+ }
+
+ async function fetchData() {
+   const respons = await fetch(url);
+   list = await response.json();
+   console.log(list);
+   showList();
+ }
+
+ function showList() {
+    let
+    main.innerHTML = <h2>Actor List</h2>;
+ }
+
+ showList();
