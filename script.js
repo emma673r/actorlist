74a36d4fe2d1682de8ebf2161c3d228b99dd8d79
emma673r <90177139+emma673r@users.noreply.github.com>
2022-08-25 09:23:46 +0200

    caroline method

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const url = "actors.json";
+
 const actorList = document.getElementById("actor");
-
 const actorDetailsContent = document.querySelector(".actor-details-content");
-
 const sortBtn = document.querySelectorAll(".sort_btn");
 const popup = document.querySelector("#popup");
 const close = document.querySelector("#close");
@@ -42,25 +41,24 @@ async function fetchData() {
 }
 
 function showList() {
-actor.textContent = "";
+  actor.textContent = "";
   list.forEach((actor) => {
-        if (
+    if (
       // if movie category is all :
-      (movie == "all") ||
+      movie == "all" ||
       // or
       // if movie cat is specified
-      (movie == actor.movie)
+      movie == actor.movie
     ) {
-    btn = document.createElement("button");
-    btn.setAttribute("class", "actor");
-    // btn.setAttribute("data-id", actor.fullname);
-    btn.textContent = actor.fullname;
-    actorList.appendChild(btn);
-    btn.addEventListener("click", () => getActorDetails(actor));
+      btn = document.createElement("button");
+      btn.setAttribute("class", "actor");
+      // btn.setAttribute("data-id", actor.fullname);
+      btn.textContent = actor.fullname;
+      actorList.appendChild(btn);
+      btn.addEventListener("click", () => getActorDetails(actor));
     }
   });
 }
-
 
 function getActorDetails(actor) {
   console.log(actor);
@@ -70,5 +68,61 @@ function getActorDetails(actor) {
           <p class = "actor-movie">You guessed it ! <b>${actor.movie}</b></p>`;
 }
 
-
 close.addEventListener("click", () => (popup.style.display = "none"));
+
+//********** Caroline's code -- different methods (not the same indicators obviously)
+
+// const detailed = document.querySelector(".details");
+// const button = document.querySelector(".actorButton");
+// const url = "actors.json";
+// fetch(url)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     handleData(data);
+//   });
+
+// function handleData(data) {
+//   //console.log(data);
+
+//   data.forEach(showDetails);
+// }
+
+// function showDetails(details) {
+//   //console.log(details);
+
+//   //grab template
+//   const template = document.querySelector("#actorFilm").content;
+//   //clone it
+//   const copy = template.cloneNode(true);
+//   //change it
+//   copy.querySelector(".actorName").textContent = details.fullname;
+//   copy.querySelector(".filmTitle").textContent = details.movie;
+//   copy.querySelector("article").addEventListener("click", showFilm);
+//   //grab parent
+//   const parent = document.querySelector("main");
+//   parent.appendChild(copy);
+//   //append child
+// }
+
+// function showFilm() {
+//   const details = this.querySelector(".details");
+//   if (details.classList.contains("show")) details.classList.remove("show");
+//   else if (document.querySelector(".show")) {
+//     document.querySelector(".show").classList.remove("show");
+//     details.classList.add("show");
+//   } else {
+//     details.classList.add("show");
+//   }
+
+//   //this.querySelector(".details").classList.toggle("show");
+// }
+
+// // function toggledetails() {
+// //   if (detailed.classList.contains("hide")) {
+// //     detailed.classList.remove("hide");
+// //     detailed.classList.add("show");
+// //   } else {
+// //     detailed.classList.remove("show");
+// //     detailed.classList.add("hide");
+// //   }
+// // }
